198daf9f5d36c5d4f5bfb1dd13a9ff752ff045ef
JhosefRea <jhosefrea18jr17@gmail.com>
2025-08-11 16:53:18 +0000

    I update the exception handler for HTTPException and when errors are of type 500

--- src/common/http-exception.filter.utils.ts
+++ src/common/http-exception.filter.utils.ts
@@ -2,6 +2,7 @@ import {
   ExceptionFilter,
   Catch,
   ArgumentsHost,
+  HttpException,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 
@@ -11,13 +12,23 @@ export class GlobalExceptionFilter implements ExceptionFilter {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
-    const status = exception.getStatus();
-    const message = exception.getResponse();
 
-    response.status(status).json({
-      message,
-      path_url: request.url,
-      //error: exception.stack,
-    });
+    if (exception instanceof HttpException) {
+      const status = exception.getStatus(); 
+      const message = exception.getResponse();  
+      response.status(status).json({
+        message,
+        path_url: request.url,
+        //error: exception.stack,  // Stack trace for debugging
+      });
+    } else {
+      // If the exception is not an HttpException, handle it as a generic error (a 500 error is assumed)
+      const message = exception.message || 'Internal server error';
+      response.status(500).json({
+        message,
+        path_url: request.url,
+        //error: exception.stack,  // Stack trace for debugging
+      });
+    }
   }
 }
